ff4f8dd372d11b155906443f21811f0889b5638c
Dev-Isaque <isaquesbl123@gmail.com>
2022-12-07 16:22:54 -0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,10 +66,8 @@ window.addEventListener("scroll", reveal);
 
 // Habilidades função
 
-jQuery(document).ready(function() {
-  jQuery('.skillbar').each(function() {
-      jQuery(this).find('.skillbar-bar').animate({ width: jQuery(this).attr('data-percent') }, 3000);
+$(document).ready(function() {
+  $('.skillbar').each(function() {
+      $(this).find('.skillbar-bar').animate({ width: $(this).attr('data-percent') }, 3000);
   });
 });
-
-
